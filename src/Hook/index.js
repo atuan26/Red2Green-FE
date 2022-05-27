c43c4747359782912dc1d36f67046c5e8d44a4b7
atuan26 <natuann26@gmail.com>
2022-05-27 16:34:54 +0700

    fix RBC toolbar re-render issue, add usePrevious hook

--- src/Hook/index.js
+++ src/Hook/index.js
@@ -30,3 +30,11 @@ export function useComponentVisible(initialIsVisible) {
 
   return { ref, isComponentVisible, setIsComponentVisible };
 }
+
+export function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value; //assign the value of ref to the argument
+  }, [value]); //this code will run when the value of 'value' changes
+  return ref.current; //in the end, return the current ref value.
+}
--- src/components/Pages/Calendar/RBCCustomComponent/index.js
+++ src/components/Pages/Calendar/RBCCustomComponent/index.js
@@ -1,8 +1,10 @@
 import moment from 'moment';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { BsCalendarDay, BsCalendarMonth, BsCalendarWeek } from 'react-icons/bs';
 import { MdOutlineNavigateBefore, MdOutlineNavigateNext, MdOutlineToday, MdOutlineViewAgenda } from 'react-icons/md';
 import { HiOutlineChevronDoubleLeft, HiOutlineChevronDoubleRight } from 'react-icons/hi';
+import { usePrevious } from '../../../../Hook';
+import { current } from 'daisyui/colors';
 
 
 export const MonthEvent = ({ event }) => {
@@ -88,158 +90,164 @@ export const AgendaEvent = ({ event }) => {
 
 export const RBCToolbar = (props) => {
   const [viewState, setViewState] = useState("month");
-  const getCustomToolbar = () => {
-    const goToDayView = () => {
-      props.onView("day");
-      setViewState("day");
-    };
-    const goToWeekView = () => {
-      props.onView("week");
-      setViewState("week");
-    };
-    const goToMonthView = () => {
-      props.onView("month");
-      setViewState("month");
-    };
-    const goToAgendaView = () => {
-      props.onView("agenda");
-      setViewState("agenda");
-    };
-    const goToBack = () => {
-      let view = viewState;
-      let mDate = props.date;
-      let newDate;
-      if (view === "month") {
-        newDate = new Date(mDate.getFullYear(), mDate.getMonth() - 1, 1);
-      } else if (view === "week") {
-        newDate = new Date(
-          mDate.getFullYear(),
-          mDate.getMonth(),
-          mDate.getDate() - 7,
-          1
-        );
-      } else {
-        newDate = new Date(
-          mDate.getFullYear(),
-          mDate.getMonth(),
-          mDate.getDate() - 1,
-          1
-        );
-      }
-      props.onNavigate("prev", newDate);
-    };
-    const goToNext = () => {
-      let view = viewState;
-      let mDate = props.date;
-      let newDate;
-      if (view === "month") {
-        newDate = new Date(mDate.getFullYear(), mDate.getMonth() + 1, 1);
-      } else if (view === "week") {
-        newDate = new Date(
-          mDate.getFullYear(),
-          mDate.getMonth(),
-          mDate.getDate() + 7,
-          1
-        );
-      } else {
-        newDate = new Date(
-          mDate.getFullYear(),
-          mDate.getMonth(),
-          mDate.getDate() + 1,
-          1
-        );
-      }
-      props.onNavigate("next", newDate);
-    };
+  const goToDayView = useCallback(() => {
+    props.onView("day");
+    setViewState("day");
+  }, [])
+  const goToWeekView = useCallback(() => {
+    props.onView("week");
+    setViewState("week");
+  }, [])
+  const goToMonthView = useCallback(() => {
+    props.onView("month");
+    setViewState("month");
+  }, [])
+  const goToAgendaView = useCallback(() => {
+    props.onView("agenda");
+    setViewState("agenda");
+  }, [])
 
-    const goToToday = () => {
-      const now = new Date();
-      props.date.setMonth(now.getMonth());
-      props.date.setYear(now.getFullYear());
-      props.date.setDate(now.getDate());
-      props.onNavigate("current");
-    };
-
-    const goToBackYear = () => {
-      let mDate = props.date;
-      let newDate = new Date(mDate.getFullYear() - 1, mDate.getMonth());
-      props.onNavigate("prev", newDate);
-    };
-
-    const goToNextYear = () => {
-      let mDate = props.date;
-      let newDate = new Date(mDate.getFullYear() + 1, mDate.getMonth());
-      props.onNavigate("next", newDate);
-    };
-
-    const ButtonTabView = ({ label, icon, onClick, isActive }) => {
-      const currentViewButtonClass = "btn btn-sm capitalize !border-transparent !h-reset !cursor-pointer !bg-[#1c64f2] text-white !px-4 !py-1 !rounded-md hover:opacity-90"
-      const otherButtonClass = "capitalize cursor-pointer bg-transparent text-[#1c64f2] px-4 py-1 rounded-md hover:opacity-90"
-      return (
-        <div
-          className={isActive ? currentViewButtonClass : otherButtonClass}
-          onClick={onClick}
-        >
-          <span className="">
-            <div className='flex items-center gap-2 '>{icon}{label}</div>
-          </span>
-        </div>)
+  const goToBack = useCallback(() => {
+    let view = viewState;
+    let mDate = props.date;
+    let newDate;
+    if (view === "month") {
+      newDate = new Date(mDate.getFullYear(), mDate.getMonth() - 1, 1);
+    } else if (view === "week") {
+      newDate = new Date(
+        mDate.getFullYear(),
+        mDate.getMonth(),
+        mDate.getDate() - 7,
+        1
+      );
+    } else {
+      newDate = new Date(
+        mDate.getFullYear(),
+        mDate.getMonth(),
+        mDate.getDate() - 1,
+        1
+      );
     }
-    const NavigateButton = ({ icon, onClick }) => {
-      return (
-        <button
-          className="btn  btn-sm flex items-center gap-2 bg-white text-[#1c64f2] !rounded-md border-[1px] border-gray-200 hover:!bg-[#1c64f2] hover:!text-white hover:!border-white hover:!rounded-md focus:!ring-2 focus:!ring-[#1c64f262]"
-          onClick={onClick}>
-          {icon}
-        </button>
-      )
+    props.onNavigate("prev", newDate);
+  }, [])
+  const goToNext = useCallback(() => {
+    let view = viewState;
+    let mDate = props.date;
+    let newDate;
+    if (view === "month") {
+      newDate = new Date(mDate.getFullYear(), mDate.getMonth() + 1, 1);
+    } else if (view === "week") {
+      newDate = new Date(
+        mDate.getFullYear(),
+        mDate.getMonth(),
+        mDate.getDate() + 7,
+        1
+      );
+    } else {
+      newDate = new Date(
+        mDate.getFullYear(),
+        mDate.getMonth(),
+        mDate.getDate() + 1,
+        1
+      );
     }
+    props.onNavigate("next", newDate);
+  }, [])
 
+  const goToToday = useCallback(() => {
+    const now = new Date();
+    props.date.setMonth(now.getMonth());
+    props.date.setYear(now.getFullYear());
+    props.date.setDate(now.getDate());
+    props.onNavigate("current");
+  }, [])
 
-    return (
-      <div className="rbc-toolbar">
-        <div className="mb-4 flex items-center justify-between w-full">
-          <div className='flex gap-1'>
-            <NavigateButton icon={<HiOutlineChevronDoubleLeft className='w-4 h-4' />} onClick={goToBackYear} />
-            <NavigateButton icon={<MdOutlineNavigateBefore className='w-4 h-4' />} onClick={goToBack} />
-            <NavigateButton icon={<MdOutlineToday className='w-4 h-4' />} onClick={goToToday} />
-            <NavigateButton icon={<MdOutlineNavigateNext className='w-4 h-4' />} onClick={goToNext} />
-            <NavigateButton icon={<HiOutlineChevronDoubleRight className='w-4 h-4' />} onClick={goToNextYear} />
-          </div>
-          <div className='text-2xl text-[#25396f] font-semibold'>
-            {props.label}{' '}
-            {("week" === viewState || 'day' === viewState) && moment(props.date).format(', YYYY')}
-          </div>
-          <div className="bg-[#1c64f226] rounded-md flex gap-2 p-1">
-            <ButtonTabView
-              label="Month"
-              isActive={"month" === viewState}
-              icon={<BsCalendarMonth className='w-4 h-4' />}
-              onClick={goToMonthView}
-            />
-            <ButtonTabView
-              label="Week"
-              isActive={"week" === viewState}
-              icon={<BsCalendarWeek className='w-4 h-4' />}
-              onClick={goToWeekView}
-            />
-            <ButtonTabView
-              label="Day"
-              isActive={"day" === viewState}
-              icon={<BsCalendarDay className='w-4 h-4' />}
-              onClick={goToDayView}
-            />
-            <ButtonTabView
-              label="Agenda"
-              isActive={"agenda" === viewState}
-              icon={<MdOutlineViewAgenda className='w-4 h-4' />}
-              onClick={goToAgendaView}
-            />
-          </div>
+  const goToBackYear = useCallback(() => {
+    let mDate = props.date;
+    let newDate = new Date(mDate.getFullYear() - 1, mDate.getMonth());
+    props.onNavigate("prev", newDate);
+  }, [])
+
+  const goToNextYear = useCallback(() => {
+    let mDate = props.date;
+    let newDate = new Date(mDate.getFullYear() + 1, mDate.getMonth());
+    props.onNavigate("next", newDate);
+  }, [])
+  return (
+    <div className="rbc-toolbar">
+      <div className="mb-4 flex items-center justify-between w-full">
+        <div className='flex gap-1'>
+          <NavigateButton type={1} onClick={goToBackYear} />
+          <NavigateButton type={2} onClick={goToBack} />
+          <NavigateButton type={3} onClick={goToToday} />
+          <NavigateButton type={4} onClick={goToNext} />
+          <NavigateButton type={5} onClick={goToNextYear} />
+        </div>
+        <div className='text-2xl text-[#25396f] font-semibold'>
+          {props.label}{' '}
+          {("week" === viewState || 'day' === viewState) && moment(props.date).format(', YYYY')}
+        </div>
+        <div className="bg-[#1c64f226] rounded-md flex gap-2 p-1">
+          <ButtonTabView
+            type="month"
+            isActive={"month" === viewState}
+            onClick={goToMonthView}
+          />
+          <ButtonTabView
+            type="week"
+            isActive={"week" === viewState}
+            onClick={goToWeekView}
+          />
+          <ButtonTabView
+            type="day"
+            isActive={"day" === viewState}
+            onClick={goToDayView}
+          />
+          <ButtonTabView
+            type="agenda"
+            isActive={"agenda" === viewState}
+            onClick={goToAgendaView}
+          />
         </div>
       </div>
-    );
-  };
-
-  return <>{getCustomToolbar()}</>;
+    </div>
+  );
 }
+
+const ButtonTabView = memo(({ type, onClick, isActive }) => {
+
+  const btnIcon = useMemo(() => ({
+    'month': <BsCalendarMonth className='w-4 h-4' />,
+    'week': <BsCalendarWeek className='w-4 h-4' />,
+    'day': <BsCalendarDay className='w-4 h-4' />,
+    'agenda': <MdOutlineViewAgenda className='w-4 h-4' />,
+  }))
+  const currentViewButtonClass = "btn btn-sm capitalize !border-transparent !bg-[#1c64f2] text-white !px-4 !py-1 !rounded-md hover:opacity-90"
+  const otherButtonClass = "capitalize cursor-pointer bg-transparent text-[#1c64f2] px-4 py-1 rounded-md hover:opacity-90"
+  return (
+    <div
+      className={isActive ? currentViewButtonClass : otherButtonClass}
+      onClick={onClick}
+    >
+      <span className="">
+        <div className='flex items-center gap-2 '>{btnIcon[type]}{type}</div>
+      </span>
+    </div>)
+})
+
+const NavigateButton = memo(({ type, onClick }) => {
+  const btnIcon = useMemo(() => ({
+    1: <HiOutlineChevronDoubleLeft className='w-4 h-4' />,
+    2: <MdOutlineNavigateBefore className='w-4 h-4' />,
+    3: <MdOutlineToday className='w-4 h-4' />,
+    4: <MdOutlineNavigateNext className='w-4 h-4' />,
+    5: <HiOutlineChevronDoubleRight className='w-4 h-4' />,
+  }))
+  return (
+    <button
+      className="btn  btn-sm flex items-center gap-2 bg-white text-[#1c64f2] !rounded-md border-[1px] border-gray-200 hover:!bg-[#1c64f2] hover:!text-white hover:!border-white hover:!rounded-md focus:!ring-2 focus:!ring-[#1c64f262]"
+      onClick={onClick}>
+      {btnIcon[type]}
+    </button>
+  )
+})
